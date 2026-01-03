1475fb6a4f11cc7f169062b989f482feeaa9e21e
LAMAsCo <LuisAlmeida.LAMAs@gmail.com>
2026-01-03 11:13:18 -0600

    Style: Remove background circle shadows

--- App.tsx
+++ App.tsx
@@ -163,23 +163,9 @@ const App: React.FC = () => {
     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
       <HashRouter>
         <ScrollToTop />
-        <div className="min-h-screen text-[#2C3E2D] dark:text-[#E2E8D5] font-sans flex flex-col selection:bg-[#D4A373] selection:text-white">
+        <div className="min-h-screen bg-[#F3F4F1] dark:bg-[#1A1C18] text-[#2C3E2D] dark:text-[#E2E8D5] font-sans transition-colors duration-700 selection:bg-[#D4A373] selection:text-white flex flex-col">
 
-          {/* Background Layer */}
-          <div className="fixed inset-0 bg-[#F3F4F1] dark:bg-[#1A1C18] z-[-2] transition-colors duration-700"></div>
 
-          {/* Noise Overlay to fix banding */}
-          <div className="fixed inset-0 z-[-2] opacity-[0.03] pointer-events-none mix-blend-overlay"
-            style={{
-              backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
-            }}
-          ></div>
-
-          {/* Global Organic Background Shapes */}
-          <div className="fixed inset-0 overflow-hidden pointer-events-none z-[-1]">
-            <div className="absolute -top-[10%] -right-[10%] w-[50vw] h-[50vw] bg-[#E5E7E1] dark:bg-[#232620] rounded-full blur-[128px] opacity-60 animate-float"></div>
-            <div className="absolute top-[40%] -left-[10%] w-[40vw] h-[40vw] bg-[#D8DCD3] dark:bg-[#2A2E26] rounded-full blur-[128px] opacity-60 animate-float" style={{ animationDelay: '2s' }}></div>
-          </div>
 
           <NavBar />
 
